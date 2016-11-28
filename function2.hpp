c2153ebed9879e96b3075649a1ca699895dcc84e
ankit chakraborty <ankit@ubuntu>
2016-11-28 22:44:07 +0100

    functions2

---
+++ function2.hpp
@@ -0,0 +1,18 @@
+#ifndef  FUNCTIONS_H_INCLUDED
+#define  FUNCTIONS_H_INCLUDED
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <math.h>
+#include "PROTOTYPE.h"
+#include "functions.hpp"
+
+template<typename T,typename U>
+void Run(U &A,T &B)
+{
+  int nx,ord;
+  double eps;
+  nx=A[0];
+  eps=B[1];
+  ord=A[6];
+}
